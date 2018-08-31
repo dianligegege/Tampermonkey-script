42780b9b02e1ba7730c7eeb28ed47405d50ee2d3
dianligegege <1220671124@qq.com>
2018-09-01 00:06:53 +0800

    豆瓣下载0.1

---
+++ 豆瓣TOP250电影下载.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         豆瓣TOP250电影下载
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  添加一个一键查找豆瓣电影TOP250（https://movie.douban.com/top250）的资源的按钮。
+// @author       dianli
+// @supportURL   dianligegege@163.com
+// @downloadURL
+// @match        https://movie.douban.com/top250
+// @grant        none
+// @require       https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+   //'use strict';
+   window.setTimeout(function() {
+//       alert('Hello world!');
+       $(".bd").find("p:last").append("<spend class='tmdownload '><a>下载</a></spend>");
+       //添加下载标签
+       $(".tmdownload").css({
+				"color":"#bbbbbb",
+				"font-size":"12px",
+				"text-align":"center",
+				"cursor":"pointer"
+		});
+       //鼠标移入效果
+       $(".tmdownload a").hover(function(){
+			$(this).css({
+			"color": "#ffffff",
+		//	"display":"inline-block",
+			"border-left": "1px solid #FF9999",
+			'border-top': '1px solid #FF9999',
+			'border-right': '1px solid #FF3333',
+			'border-bottom': '1px solid #FF3333',
+			'background-color': '#773333',
+			'text-align': 'center'
+			})
+		},function(){
+           $(this).css({
+			"color": "#bbbbbb",
+			"font-size": "12px",
+			"text-align": "center",
+			"cursor": "pointer",
+            'background-color': '#ffffff',
+			"border":"none"
+               })
+		});
+       //网页跳转
+       $(".tmdownload").click(function(){
+			var title=$(this).parent().parent().prev().find('.title').first().text();
+			//console.log(title);
+           	window.open('http://www.dysfz.cc/key/'+title+'/');
+		});
+   }, 60);
+    
+ })();

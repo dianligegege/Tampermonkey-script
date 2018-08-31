44dcf9e074520441d5feccb3930144f2d3214705
dianligegege <1220671124@qq.com>
2018-09-01 00:13:23 +0800

    add download url

--- 豆瓣TOP250电影下载.user.js
+++ 豆瓣TOP250电影下载.user.js
@@ -5,7 +5,7 @@
 // @description  添加一个一键查找豆瓣电影TOP250（https://movie.douban.com/top250）的资源的按钮。
 // @author       dianli
 // @supportURL   dianligegege@163.com
-// @downloadURL
+// @downloadURL		https://github.com/dianligegege/Tampermonkey-script
 // @match        https://movie.douban.com/top250
 // @grant        none
 // @require       https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
